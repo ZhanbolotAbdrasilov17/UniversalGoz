f6a1de360e3457a9a00f1cac9b03f9d331643bd3
Zhanbolot Abdrasilov <zhanbolot19971997@gmail.com>
2022-05-18 17:51:33 +0600

    second commit

--- home/models.py
+++ home/models.py
@@ -15,3 +15,17 @@ class Description(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name="descriptions")
     text = models.TextField(verbose_name="Описание")
 
+
+class News(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Новости")
+    date = models.DateField(auto_now_add=True)
+    image = models.ImageField(null=True, blank=True, upload_to="news")
+
+    def __str__(self):
+        return self.title
+
+class Fulldescription(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name="news_descriptions")
+    text = models.TextField(verbose_name="Текст")
+
+
